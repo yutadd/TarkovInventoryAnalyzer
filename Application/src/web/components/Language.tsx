64e0afa2b843a3fff2e0450736cb101a06ae0ace
yutadd <spyk4m49@gmail.com>
2024-12-24 20:13:49 +0900

    [push] UI improvement by yamane

--- Application/src/web/components/Language.tsx
+++ Application/src/web/components/Language.tsx
@@ -2,17 +2,19 @@ import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
 import './Language.css'
 export const Language = () => {
-    const context = useContext(AppContext)
-    const [itemList, setItemList] = useState<JSX.Element[]>([])
+    const context = useContext(AppContext);
+    const [itemList, setItemList] = useState<JSX.Element[]>([]);
 
     useEffect(() => {
-
+        
     }, [context.dateKeyedItemGroup]);
-    return (<div className="LanguageContainer">
-        <select value={context.selectedLanguage} onChange={(e) => context.setSelectedLanguage(e.target.value)}>
-            <option value="ja">日本語</option>
-            <option value="en">English</option>
-        </select>
-    </div>
+
+    return (
+        <div className="LanguageContainer">
+            <select className="Language" value={context.selectedLanguage} onChange={(e) => context.setSelectedLanguage(e.target.value)}>
+                <option value="ja">日本語</option>
+                <option value="en">English</option>
+            </select>
+        </div>
     );
 };
